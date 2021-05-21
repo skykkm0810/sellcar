1aa92ed4126281a9454d4271c7da550bb211151c
skykkm0810 <skykkm0810@naver.com>
2021-05-21 15:16:07 +0900

    check

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const fs = require('fs');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.get('./image',(req, res)=>{
+  fs.readFile('trailblazer.png', (error,data)=>{
+    res.writeHead(200, { 'content-type' :'text/html' });
+    res.end(data);
+  });
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!1')
+  // res.send('Hello World!1')
+  res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(port, () => {
--- test/test.js
+++ test/test.js
@@ -1,10 +1,24 @@
 var assert = require("assert"); //nodejs에서 제공하는 aseert 모듈
+const request = require('superagent');
 
-describe('Array 테스트', function() {
-	describe('indexOf() 메서드', function () {
-		it('값이 없을 때에는 -1 을 리턴함', function () {
-			assert.equal(-1, [1,2,3].indexOf(5));
-			assert.equal(-1, [1,2,3].indexOf(0));
+describe('신차판매 메인페이지 테스트', function() {
+
+	   
+	describe('신차판매문구 존재 여부', function () {
+		it('response result check', function () {
+			var url = 'http://localhost:3000/';
+			request.get(url).end(function(err, res){
+				assert.strictEqual(res.ok, true);
+				assert.strictEqual(res.statusCode, 200);
+			})
 		});
 	});
+
+	describe('indexOf() 메서드', function () {
+		it('값이 없을 때에는 -1 을 리턴함', function () {
+			assert.strictEqual(-1, [1,2,3].indexOf(5));
+			assert.strictEqual(-1, [1,2,3].indexOf(0));
+		});
+	});
+	
 });
